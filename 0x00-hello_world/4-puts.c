945f6a141fffbb2e17280766a8ffaba6baeaf175
Luwato <oluwadare.crown@gmail.com>
2022-06-02 17:08:54 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -14,11 +14,10 @@ int main(void)
 
 {
 
-	        char displayed[52];
-
-		        strcpy(displayed, "\"Programming is like building a multilingual puzzle""
-
-			      );
+   char displayed[52];
+strcpy(displayed, "\"Programming is like building a multilingual puzzle""
+			     
+      );
 
 			        puts(displayed);
 
