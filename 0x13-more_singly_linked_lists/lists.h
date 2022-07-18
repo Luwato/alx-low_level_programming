5411b63406c1ae3e4fdf855667d5e18f1af4eed8
Luwato <oluwadare.crown@gmail.com>
2022-07-18 11:07:40 -0700

    prototypes

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,11 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+/*
+ * Desc: Header file containing prototypes and definitions for all functions
+ *       and types written in the 0x12-more_singly_linked_lists directory.
+ */
+
 #include <stdlib.h>
 
 /**
@@ -9,7 +14,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ * for ALX project
  */
 typedef struct listint_s
 {
@@ -33,4 +38,4 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-#endif
+#endif /* LISTS_H */
